c0eb4b415167ab09281edc0118d03bf0a36605c0
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-04-15 22:46:01 +0200

    new start

---
+++ js1.1.js
@@ -0,0 +1,13 @@
+  function toiletPaperCalculator() {
+    
+    let numberOfRolls = document.getElementById("input4Rolls").value;
+    let frequency = document.getElementById("input4Frequency").value;
+    let numberOfSheets = document.getElementById("input4Sheets").value;
+    let days =  (400*numberOfRolls) / (frequency*numberOfSheets);
+    if (days >= 1 ) {
+      document.getElementById('calculatorOutput').innerHTML = "You can enjoy your time on the toilet for " + days + "more days !";
+    }
+    else {
+      document.getElementById('calculatorOutput').innerHTML = "You just have a bit paper left, good luck finding new paper !";
+    }
+  }
