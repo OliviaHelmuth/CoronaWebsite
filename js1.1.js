21cbfc01b1bcf2f4a0915b2f244248143316e84a
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-04-19 12:54:04 +0200

    logic fix

--- js1.1.js
+++ js1.1.js
@@ -3,7 +3,7 @@
     let numberOfRolls = document.getElementById("input4Rolls").value;
     let frequency = document.getElementById("input4Frequency").value;
     let numberOfSheets = document.getElementById("input4Sheets").value;
-    let days =  (400*numberOfRolls) / (frequency*numberOfSheets);
+    let days =  (300*numberOfRolls) / (frequency*numberOfSheets);
     if (days >= 1 ) {
       document.getElementById('calculatorOutput').innerHTML = "You can enjoy your time on the toilet for " + Math.round(days) + " more days !";
     }
