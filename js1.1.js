5f2ea98fa54968f420e8b73b2b24b93f06b9846b
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-04-19 13:56:28 +0200

    enhance calculator logic

--- js1.1.js
+++ js1.1.js
@@ -1,13 +1,20 @@
-  function toiletPaperCalculator() {
-    
+const toiletForm = document.getElementById("toilet-form");
+toiletForm.addEventListener("submit", toiletPaperCalculator)
+
+  function toiletPaperCalculator(e) {
+    e.preventDefault()
     let numberOfRolls = document.getElementById("input4Rolls").value;
     let frequency = document.getElementById("input4Frequency").value;
     let numberOfSheets = document.getElementById("input4Sheets").value;
     let days =  (300*numberOfRolls) / (frequency*numberOfSheets);
-    if (days >= 1 ) {
+    if (numberOfRolls == 0) {
+      document.getElementById('calculatorOutput').innerHTML = "You don't have any toilet paper left!";
+    }
+    else if (days >= 1 ) {
       document.getElementById('calculatorOutput').innerHTML = "You can enjoy your time on the toilet for " + Math.round(days) + " more days !";
     }
     else {
       document.getElementById('calculatorOutput').innerHTML = "You just have a bit paper left, good luck finding new paper !";
     }
   }
+
